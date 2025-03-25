11421675e8980d7cb9eb4da97ea20e7a5bae7823
Rafael José <rafaeljdm21@hotmail.com>
2025-03-24 22:13:34 -0300

    Update PromotionCard component: adjust dimensions, enhance layout, and add address display for promotions

--- frontend/src/components/PromotionCard.jsx
+++ frontend/src/components/PromotionCard.jsx
@@ -4,14 +4,14 @@ const PromotionCard = ({ promotion }) => {
   return (
     <div
       className="bg-gray-800 text-white shadow-lg rounded-lg p-6 border border-gray-700 border-2 border-yellow-500
-      md:h-48 min-h-[364px] md:min-h-[200px] flex flex-col justify-between mx-auto max-w-[95%] md:max-w-full"
+      md:h-72 min-h-[364px] md:min-h-[288px] flex flex-col justify-between mx-auto max-w-[95%] md:max-w-full"
     >
       <h2 className="text-xl md:text-xl font-bold text-center pb-3 border-b border-gray-600">
         {promotion.restaurant_name}
       </h2>
 
       <div className="flex flex-row items-center flex-grow pt-6 md:pt-4">
-        <div className="flex-shrink-0 w-24 md:w-20 h-24 md:h-20 flex items-center justify-center">
+        <div className="flex-shrink-0 w-25 md:w-30 h-25 md:h-30 flex items-center justify-center">
           {promotion.logo_path && (
             <img
               src={`${import.meta.env.VITE_API_URL}/${promotion.logo_path}`}
@@ -25,8 +25,11 @@ const PromotionCard = ({ promotion }) => {
           <p className="mb-4 md:mb-2 text-base">
             <strong>Descuento:</strong> {promotion.discount_rate ?? "N/A"}
           </p>
-          <p className="text-base">
+          <p className="mb-0 md:mb-2 text-base">
             <strong>Validez:</strong> {promotion.valid_period_text ?? "N/A"}
+          </p>
+          <p className="hidden md:block text-base">
+            <strong>Dirección:</strong> {promotion.address ?? "N/A"}
           </p>
         </div>
       </div>
